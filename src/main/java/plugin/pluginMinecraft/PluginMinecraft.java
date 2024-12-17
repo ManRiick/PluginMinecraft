17503c93126a36daafd846dcc1890fc8c116b939
Manukyan <astghik.manukyan@lycee-merleauponty.fr>
2024-12-17 18:30:12 +0100

    message d'activation/desactivation de serveur

--- src/main/java/plugin/pluginMinecraft/PluginMinecraft.java
+++ src/main/java/plugin/pluginMinecraft/PluginMinecraft.java
@@ -7,12 +7,13 @@ public final class PluginMinecraft extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        
+        System.out.println("Salut! Le serveur est activé!!!");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shut down logic
+        System.out.println("Salut! Le serveur est désactivé!!!");
     }
 }
