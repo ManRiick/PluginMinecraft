f010e5f11e318755f83b9370fe3f74b5206eceda
Manukyan <astghik.manukyan@lycee-merleauponty.fr>
2024-12-17 17:04:33 +0100

    Initial commit

---
+++ src/main/java/plugin/pluginMinecraft/PluginMinecraft.java
@@ -0,0 +1,18 @@
+package plugin.pluginMinecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginMinecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shut down logic
+    }
+}
